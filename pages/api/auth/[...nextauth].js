8fc31956b89b398baae3cb11a7044b7860cdbe10
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-02-17 19:46:12 -0500

    Next Auth WIP

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,9 +2,7 @@ import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from '../../../lib/prisma';
 
 export default NextAuth({
   providers: [
